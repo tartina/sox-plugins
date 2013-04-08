b127235436cbc2164d5d4e4d4663ac8372b35361
Guido Aulisi <guido.aulisi@gmail.com>
2013-04-08 15:16:59 +0200

    Limiter.c: correct email

--- limiter.c
+++ limiter.c
@@ -1,6 +1,6 @@
 /*
     LibSox limiter
-    Copyright (C) 2013 Guido Aulisi <guido.aulisi@gmail.com
+    Copyright (C) 2013 Guido Aulisi guido.aulisi@gmail.com
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
